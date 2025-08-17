b35489af3e4adf5d4a304f3ff6c03708854d2b3f
sammyklan3 <sammyklane7@gmail.com>
2025-08-17 07:55:49 +0300

    Fix: Add parameter checks when creating api keys

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -2,9 +2,9 @@ import { createApiKey } from "../services/api.service.js";
 
 // Controller for creating an api key
 async function createApiKeyController(req, res) {
-    const userId = req.user.id;
-    const apiKey = await createApiKey(userId);
-    res.status(201).json(apiKey);
-};
+  const userId = req.user.id;
+  const apiKey = await createApiKey(userId);
+  res.status(201).json(apiKey);
+}
 
 export { createApiKeyController };
