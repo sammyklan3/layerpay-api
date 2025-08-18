35e45743608cb6e0cba1ee5f74b02096741c7716
sammyklan3 <sammyklane7@gmail.com>
2025-08-18 08:52:17 +0300

    Core: Add api key revocation controller to routes

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,4 +1,4 @@
-import { createApiKey, getApiKeys } from "../services/api.service.js";
+import { createApiKey, getApiKeys, revokeApiKey } from "../services/api.service.js";
 
 // Controller for creating an api key
 async function createApiKeyController(req, res) {
@@ -14,4 +14,12 @@ async function getApiKeysController(req, res) {
   res.status(200).json(apiKeys);
 }
 
-export { createApiKeyController, getApiKeysController };
+// Controller for revoking api keys
+async function revokeApiKeyController(req, res) {
+  const userId = req.user.id;
+  const apiKey = req.params.apiKey;
+  await revokeApiKey(userId, apiKey);
+  res.status(204).send();
+};
+
+export { createApiKeyController, getApiKeysController, revokeApiKeyController };
